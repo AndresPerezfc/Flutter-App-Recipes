bc85aaed1da6d41cac6a61a0be45e73e4bf69e54
andresperezfc <felipe3apc@gmail.com>
2020-04-12 11:36:02 -0500

    1 - Configuración del proyecto

---
+++ lib/model/receta_model.dart
@@ -0,0 +1,8 @@
+class Receta {
+  Receta({this.uid, this.receta, this.imagen, this.nombre});
+
+  String uid;
+  String receta;
+  String imagen;
+  String nombre;
+}
---
+++ lib/model/user_model.dart
@@ -0,0 +1,20 @@
+class Usuario {
+  Usuario(
+      {this.uid,
+      this.foto,
+      this.ciudad,
+      this.email,
+      this.nombre,
+      this.password,
+      this.telefono,
+      this.direccion});
+
+  String uid;
+  String foto;
+  String ciudad;
+  String email;
+  String nombre;
+  String password;
+  String telefono;
+  String direccion;
+}
